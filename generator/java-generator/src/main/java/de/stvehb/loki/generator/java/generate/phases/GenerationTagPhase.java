c94831c7984dbff2c230139f32fb23b6989247c4
Steve <stve.hb@gmail.com>
2020-11-25 20:00:22 +0100

    Fixed wrong package for #Generated annotation

--- generator/java-generator/src/main/java/de/stvehb/loki/generator/java/generate/phases/GenerationTagPhase.java
+++ generator/java-generator/src/main/java/de/stvehb/loki/generator/java/generate/phases/GenerationTagPhase.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class GenerationTagPhase {
 
 	private static final List<Annotation> DEFAULT_ANNOTATIONS = Arrays.asList(
-		new Annotation("Generated", "javax.annotation.processing.Generated")
+		new Annotation("Generated", "javax.annotation.processing")
 			.addValue("value", "\"" + JavaGenerator.class.getCanonicalName() + "\"")
 	);
 
